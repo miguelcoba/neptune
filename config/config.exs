5a19dda68afd5d05df76713b4245d23e415c6ecc
Miguel Coba <miguel.coba@gmail.com>
2022-05-01 23:23:12 +0200

    libcluster with Cluster.Strategy.LocalEpmd

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,13 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :libcluster,
+  topologies: [
+    local_epmd_example: [
+      strategy: Cluster.Strategy.LocalEpmd
+    ]
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
