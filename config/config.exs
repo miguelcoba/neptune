ec998c4912fd3bde4817569bb2220f9a8d7fce68
Miguel Coba <miguel.coba@gmail.com>
2022-05-01 23:36:12 +0200

    libcluster with Cluster.Strategy.ErlangHosts

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,13 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :libcluster,
+  topologies: [
+    erlang_hosts_example: [
+      strategy: Elixir.Cluster.Strategy.ErlangHosts
+    ]
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
